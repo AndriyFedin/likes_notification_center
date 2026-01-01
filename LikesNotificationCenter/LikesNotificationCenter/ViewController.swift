677e62fd4d70f44f8dbe3032891066b6ecedfd65
Andrii Fedin <andriy.fedin@yalantis.net>
2026-01-01 13:17:02 +0200

    create project

---
+++ LikesNotificationCenter/LikesNotificationCenter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LikesNotificationCenter
+//
+//  Created by Andriy Fedin on 01.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LikesNotificationCenter/LikesNotificationCenterTests/LikesNotificationCenterTests.swift
@@ -0,0 +1,17 @@
+//
+//  LikesNotificationCenterTests.swift
+//  LikesNotificationCenterTests
+//
+//  Created by Andriy Fedin on 01.01.2026.
+//
+
+import Testing
+@testable import LikesNotificationCenter
+
+struct LikesNotificationCenterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
